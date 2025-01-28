ba391099b1cf81aa10a279ce0a626735bdc7880d
Tran Minh Phu <trminhphu79@gmail.com>
2025-01-28 10:07:02 +0700

    Initial commit

---
+++ apps/phutran.info/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'phutran.info',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/phutran.info',
+};
